a531b55a982a136417c0789a0ff4fd81cfa7dd02
varasukmu <wrp.sukmuang@gmail.com>
2025-11-02 20:47:15 +0700

    Updates

--- _next/static/ZZ9FRWyg2dTtXcMFpwLHi/_buildManifest.js
+++ _next/static/ZZ9FRWyg2dTtXcMFpwLHi/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Tetris-armFit.github.io//_next/:path+",
-        "destination": "/Tetris-armFit.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
